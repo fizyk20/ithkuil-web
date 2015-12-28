3e618beb4dd250ffe0f20a80badcbcece2e8238a
Bartłomiej Kamiński <fizyk20@gmail.com>
2015-12-28 10:59:51 +0100

    Display an abbreviated analysis

--- ithkuil_app/__init__.py
+++ ithkuil_app/__init__.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'Ithkuil analyzer website under reconstruction.'
+	return render_template("index.html")
+
+@app.route('/analyze')
+def analyze():
+	from ithkuil.morphology import fromString
+	
+	word = request.args.get('word', None)
+	if not word:
+		return redirect(url_for("index"))
+	
+	wordObj = fromString(word)
+	abbr = wordObj.abbreviatedDescription()
+	
+	return render_template("analysis.html", word=word, abbr=abbr)
+
+@app.route('/error')
+def error():
+	return "An error has occured, please return to <a href=\"/\">index</a>"
